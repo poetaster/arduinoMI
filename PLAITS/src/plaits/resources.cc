586723caf35f684da5941e3563486552100fda75
ledlaux <60392231+ledlaux@users.noreply.github.com>
2025-12-15 18:59:33 +0200

    Restore to default SYX bank name numbers

--- PLAITS/src/plaits/resources.cc
+++ PLAITS/src/plaits/resources.cc
@@ -34,7 +34,7 @@
 
 namespace plaits {
 
-const uint8_t syx_bank_1[] = {
+const uint8_t syx_bank_0[] = {
       99,     58,     99,     60,
       99,      0,      0,      0,
        0,      0,      0,      0,
@@ -1060,7 +1060,7 @@ const uint8_t syx_bank_1[] = {
      117,    110,     99,    101,
       32,     52,     32,     32,
 };
-const uint8_t syx_bank_2[] = {
+const uint8_t syx_bank_1[] = {
       95,     29,     20,     50,
       99,     95,      0,      0,
       41,      0,     19,      0,
@@ -2086,7 +2086,7 @@ const uint8_t syx_bank_2[] = {
       68,     82,     77,     45,
       83,     78,     65,     82,
 };
-const uint8_t syx_bank_0[] = {
+const uint8_t syx_bank_2[] = {
       97,     99,     85,     68,
       99,     99,      0,      0,
       56,     20,      0,      2,
