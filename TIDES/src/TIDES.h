bee926379de6f76b770d31cc9fd3d74869d78c5e
Mark Washeim <blueprint@poetaster.de>
2025-12-16 09:35:27 +0100

    Refactor: add submodule  of tides

--- TIDES/src/TIDES.h
+++
@@ -1,7 +0,0 @@
-#include "tides2/ramp_generator.h"
-#include "tides2/ramp_shaper.h"
-#include "tides2/ramp_extractor.h"
-#include "tides2/ratio.h"
-#include "tides2/resources.h"
-#include "tides2/poly_slope_generator.h"
-
