a0d92bd7c3dc43b212bf127943fb0f1cfcf70169
Mark Washeim <blueprint@poetaster.de>
2025-12-20 17:50:59 +0100

    Fix: wrap patch around...;

--- PlaitsEnginesScarp1.2/PlaitsEnginesScarp1.2.ino
+++ PlaitsEnginesScarp1.2/PlaitsEnginesScarp1.2.ino
@@ -471,7 +471,8 @@ void loop1() {
       //  if ((!potlock[1]) || (!potlock[2])) seq[i].trigger=euclid(16,map(potvalue[1],POT_MIN,POT_MAX,0,MAX_SEQ_STEPS),map(potvalue[2],POT_MIN,POT_MAX,0,MAX_SEQ_STEPS-1));
       if ( i == 8 && encoder_delta != 0) {
         engineCount = engineCount + encoder_delta;
-        if (engineCount > 25) engineCount = 0; // circle around
+        if (engineCount > 24) engineCount = 0; // circle around
+        if (engineCount < 0) engineCount = 24;
         engine_in = engineCount; // ( engine +) % voices[0].voice_.GetNumEngines();
         changeEngine(engineCount);
       }
